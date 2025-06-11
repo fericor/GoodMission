38c9f66320456d2f67ce33d76a0b8a19a92bd690
Felix Cortez <fericor@hotmail.com>
2025-06-12 00:24:40 +0200

    1

--- control/db.php
+++ control/db.php
@@ -1,8 +1,8 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
     $conn = new mysqli('127.0.0.1', 'adminphp', 'Swv11w9x**', 'good_mission');
     if ($conn->connect_error) {
