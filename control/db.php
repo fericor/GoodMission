0fe6b9416c516063d7192f5464b8f16cdeef4a76
Felix Cortez <fericor@hotmail.com>
2025-06-12 00:22:03 +0200

    Inicio

--- control/db.php
+++ control/db.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-    $conn = new mysqli('127.0.0.1', 'phpmyadmin', 'tdvSwv11w9x**', 'good_mission');
+    $conn = new mysqli('127.0.0.1', 'adminphp', 'Swv11w9x**', 'good_mission');
     if ($conn->connect_error) {
         die("Conexión fallida: " . $conn->connect_error);
     }
